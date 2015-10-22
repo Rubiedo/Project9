1ee13a7eba54d7be652c168ff691ae36794bad83
Stephen Wu <wu.stephend@gmail.com>
2015-10-21 23:54:27 -0400

    initial commit

--- Project7/MasterViewController.swift
+++ Project7/MasterViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    var objects = [[String: String]]()
 
 
     override func awakeFromNib() {
@@ -20,10 +20,49 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
+        var urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            
+        } else {
+            
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            
+        }
+        
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)){ [unowned self] in
+        
+            if let url = NSURL(string: urlString){
+            
+                if let data = NSData(contentsOfURL: url, options: .allZeros, error: nil){
+                
+                    let json = JSON(data: data)
+                    if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    
+                        //start parse
+                        self.parseJSON(json)
+                    
+                    } else {
+                    
+                        self.showError()
+                    
+                    }
+                
+                } else {
+                
+                    self.showError()
+                
+                }
+            
+            } else {
+            
+                self.showError()
+            
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,18 +70,12 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
-
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let object = objects[indexPath.row] as! NSDate
+                let object = objects[indexPath.row]
             (segue.destinationViewController as! DetailViewController).detailItem = object
             }
         }
@@ -61,25 +94,43 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["body"]
         return cell
     }
-
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    
+    func parseJSON(json: JSON){
+        
+        for result in json["results"].arrayValue{
+            
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            objects.append(obj)
+            
         }
+        
+        dispatch_async(dispatch_get_main_queue()){ [unowned self] in
+        
+            self.tableView.reloadData()
+        
+        }
+        
     }
-
+    
+    func showError(){
+        
+        dispatch_async(dispatch_get_main_queue()){ [unowned self] in
+            
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(ac, animated: true, completion: nil)
+            
+        }
+        
+    }
 
 }
 
